1f3953efeb39e902c95325235e12610b6f50eb50
Wesley Soo-Hoo <wesoohoo@gmail.com>
2020-02-18 10:35:27 -0500

    start initialization

--- labs/think/think.ino
+++ labs/think/think.ino
@@ -12,9 +12,24 @@
  */
 
 #include "thinklab.h"
+#include <Servo.h>
+
+#define POT_PIN       0
+#define RUDDER_PIN    0
+#define PROPS_PIN     0
+#define TURNTABLE_PIN 0
+#define LED_PIN       0
+#define BUZZER_PIN    0
+
+Servo rudderServo;
+Servo propsServo;
+Servo turnTableServo;
 
 void setup() {
-
+  pinMode(POT_PIN, INPUT);
+  rudderServo.attach(RUDDER_PIN);
+  propsServo.attach(PROPS_PIN);
+  turnTableServo.attach(TURNTABLE_PIN);
 }
 
 void loop() {
--- labs/think/thinklab.h
+++ labs/think/thinklab.h
@@ -1,1 +1,8 @@
+// Controller functions
+void set_heading(float heading);
+void set_vel(float vel);
 
+// Act functions
+void set_rudder(float rudder_pcnt);
+void set_props(float props_pcnt);
+void set_turntable(float turntable_pcnt);
