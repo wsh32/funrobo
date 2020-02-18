33091031e58f55acee39c8c384c2e97a96b57c8c
Wesley Soo-Hoo <wesoohoo@gmail.com>
2020-02-18 09:46:22 -0500

    add base for think lab

---
+++ labs/think/think.ino
@@ -0,0 +1,20 @@
+/**
+ * FunRobo Think Lab
+ * 
+ * Team C: 
+ * David Tarazi
+ * Wesley Soo-Hoo
+ * Samuel Cabrera Valencia
+ * Forian Michael Schwarzinger
+ * Richard Gao
+ * Ben Ziemann
+ * 
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
