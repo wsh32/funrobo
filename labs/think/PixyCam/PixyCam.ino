a60181f71780b4909aa66edbc170e634b1f16b6d
Florian Schwarzinger <Firestarss88@gmail.com>
2020-02-24 00:10:35 -0500

    Added header file and updated PixyCam.ino

--- labs/think/PixyCam/PixyCam.ino
+++ labs/think/PixyCam/PixyCam.ino
@@ -1,34 +1,8 @@
-//
-// begin license header
-//
-// This file is part of Pixy CMUcam5 or "Pixy" for short
-//
-// All Pixy source code is provided under the terms of the
-// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
-// Those wishing to use Pixy source code, software and/or
-// technologies under different licensing terms should contact us at
-// cmucam@cs.cmu.edu. Such licensing terms are available for
-// all portions of the Pixy codebase presented here.
-//
-// end license header
-//
-// This sketch is a good place to start if you're just getting started with 
-// Pixy and Arduino.  This program simply prints the detected object blocks 
-// (including color codes) through the serial console.  It uses the Arduino's 
-// ICSP port.  For more information go here:
-//
-// http://cmucam.org/projects/cmucam5/wiki/Hooking_up_Pixy_to_a_Microcontroller_(like_an_Arduino)
-//
-// It prints the detected blocks once per second because printing all of the 
-// blocks for all 50 frames per second would overwhelm the Arduino's serial port.
-//
-   
-#include <SPI.h>  
-#include <Pixy.h>
+#include "thinkpixy.h"
 
 // This is the main Pixy object 
 Pixy pixy;
-int whaleData[5];
+PixyCamData data;
 
 void setup()
 {
@@ -47,7 +21,9 @@ void whaleLock()
 {
   static int i = 0;
   uint16_t blocks;
-  char buf[32]; 
+  char buf[32];
+  int mid_point = 319/2 + 1;
+  
  
   
   // grab blocks!
@@ -60,13 +36,13 @@ void whaleLock()
     // do this (print) every 50 frames because printing every
     // frame would bog down the Arduino
     if (i%25==0){
-      whaleData[0] = pixy.blocks[0].x;
-      whaleData[1] = pixy.blocks[0].y;
-      whaleData[2] = pixy.blocks[0].width;
-      whaleData[3] = pixy.blocks[0].height;
-      whaleData[4] = pixy.blocks[0].width * pixy.blocks[0].height;
+      data.x = pixy.blocks[0].x;
+      data.y = pixy.blocks[0].y;
+      data.w = pixy.blocks[0].width;
+      data.h = pixy.blocks[0].height;
+      data.a = pixy.blocks[0].width * pixy.blocks[0].height;
        
-      sprintf(buf, "x: %d\ny: %d\nwidth: %d\nheight: %d\narea: %d\n\n", whaleData[0], whaleData[1], whaleData[2], whaleData[3], whaleData[4]);
+      sprintf(buf, "x: %d\ny: %d\nwidth: %d\nheight: %d\narea: %d\n\n", data.x, data.y, data.w, data.h, data.a);
       Serial.print(buf);
     }
   }
---
+++ labs/think/PixyCam/thinkpixy.h
@@ -0,0 +1,6 @@
+#include <SPI.h>
+#include <Pixy.h>
+
+struct PixyCamData {
+  int x, y, w, h, a;
+};
